bd9bdbf339c2561d5c78962888c3fd0a0773f4b4
Crumster <crumster88@gmail.com>
2017-01-16 20:19:06 +0200

    Fix tabs/spaces

--- vision/src/main/java/Vision.java
+++ vision/src/main/java/Vision.java
@@ -10,7 +10,7 @@ public class Vision{
 	private UsbCamera camera;
 
 	// This is the network port you want to stream the raw and cv images to
-    // By rules, must be between 1180 and 1190
+	// By rules, must be between 1180 and 1190
 	private int rawStreamPort = 1185;
 	private int cvStreamPort = 1186;
 	private MjpegServer rawStream;
@@ -23,54 +23,54 @@ public class Vision{
 
 	public Vision(){
 		// Loads our OpenCV library. This MUST be included
-    	System.loadLibrary("opencv_java310");
-
-    	// Connect NetworkTables, and get access to the publishing table
-    	NetworkTable.setClientMode();
-    	// Set your team number here
-    	NetworkTable.setTeam(4338);
-    	NetworkTable.initialize();
-    	//Get the SmartDashboard networktable
-    	sd = NetworkTable.getTable("SmartDashboard");
-
+		System.loadLibrary("opencv_java310");
+		
+		// Connect NetworkTables, and get access to the publishing table
+		NetworkTable.setClientMode();
+		// Set your team number here
+		NetworkTable.setTeam(4338);
+		NetworkTable.initialize();
+		//Get the SmartDashboard networktable
+		sd = NetworkTable.getTable("SmartDashboard");
+		
 		//Initialize Camera
-    	camera = new UsbCamera("CoprocessorCamera", 0);
-    	camera.setPixelFormat(VideoMode.PixelFormat.kMJPEG);
-    	camera.setResolution(640, 480);
-    	camera.setFPS(30);
-
-    	//Initialize cv sink and source
-    	cvSink = new CvSink("CV Image Grabber");
-    	cvSink.setSource(camera);
-    	cvSource = new CvSource("CV Image Source", VideoMode.PixelFormat.kMJPEG, 640, 480, 30);
-
-    	//Initialize streams
-    	rawStream = new MjpegServer("Raw Server", rawStreamPort);
-    	rawStream.setSource(camera);
-    	cvStream = new MjpegServer("CV Server", cvStreamPort);
-    	cvStream.setSource(cvSource);
+		camera = new UsbCamera("CoprocessorCamera", 0);
+		camera.setPixelFormat(VideoMode.PixelFormat.kMJPEG);
+		camera.setResolution(640, 480);
+		camera.setFPS(30);
+		
+		//Initialize cv sink and source
+		cvSink = new CvSink("CV Image Grabber");
+		cvSink.setSource(camera);
+		cvSource = new CvSource("CV Image Source", VideoMode.PixelFormat.kMJPEG, 640, 480, 30);
+		
+		//Initialize streams
+		rawStream = new MjpegServer("Raw Server", rawStreamPort);
+		rawStream.setSource(camera);
+		cvStream = new MjpegServer("CV Server", cvStreamPort);
+		cvStream.setSource(cvSource);
 	}
-	
+
 	public void process(){
 		// All Mats and Lists should be stored outside the loop to avoid allocations
-    	// as they are expensive to create
+		// as they are expensive to create
 		Mat frame = new Mat();
 		Mat hsv = new Mat();
-
+		
 		while(true){
 			// Grab a frame. If it has a frame time of 0, there was an error.
-	      	// Just skip and continue
-	      	long frameTime = cvSink.grabFrame(frame);
-	      	if (frameTime == 0) continue;
-
-	      	// Below is where you would do your OpenCV operations on the provided image
-	      	// The sample below just changes color source to HSV
-	      	Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_BGR2HSV);
-
-	      	// Here is where you would write a processed image that you want to restreams
-	      	// This will most likely be a marked up image of what the camera sees
-	      	// For now, we are just going to stream the HSV image
-	      	cvSource.putFrame(hsv);
+			// Just skip and continue
+			long frameTime = cvSink.grabFrame(frame);
+			if (frameTime == 0) continue;
+			
+			// Below is where you would do your OpenCV operations on the provided image
+			// The sample below just changes color source to HSV
+			Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_BGR2HSV);
+			
+			// Here is where you would write a processed image that you want to restreams
+			// This will most likely be a marked up image of what the camera sees
+			// For now, we are just going to stream the HSV image
+			cvSource.putFrame(hsv);
 		}
 	}
 
