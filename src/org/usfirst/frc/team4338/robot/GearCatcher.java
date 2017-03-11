e8ccb0500d357901b46f9c5dfb51d227ef0cdd87
aaronshappell <crumster88@gmail.com>
2017-03-11 13:02:14 +0200

    Change gear catcher

--- src/org/usfirst/frc/team4338/robot/GearCatcher.java
+++ src/org/usfirst/frc/team4338/robot/GearCatcher.java
@@ -2,13 +2,15 @@ package org.usfirst.frc.team4338.robot;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Timer;
 
 public class GearCatcher{
     private DigitalInput trigger;
     private DoubleSolenoid pistons;
     
     private boolean enabled = false;
-
+    private double openTime = 5; //>3
+    
     public GearCatcher(){
         trigger = new DigitalInput(0);
         pistons = new DoubleSolenoid(0, 7);
@@ -16,10 +18,15 @@ public class GearCatcher{
 
     public void open(){
         pistons.set(DoubleSolenoid.Value.kReverse);
+        openTime = Timer.getFPGATimestamp();
     }
 
     public void close(){
         pistons.set(DoubleSolenoid.Value.kForward);
+    }
+    
+    public double getOpenTime(){
+    	return openTime;
     }
 
     public boolean getTriggerState(){
