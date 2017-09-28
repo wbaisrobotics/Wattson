7d5830080d7f42ef3f67246e30a3b5cd00339fd6
aaronshappell <crumster88@gmail.com>
2017-09-28 14:51:16 -0700

    Add speed damper

--- src/org/usfirst/frc/team4338/robot/Robot.java
+++ src/org/usfirst/frc/team4338/robot/Robot.java
@@ -635,7 +635,7 @@ public class Robot extends IterativeRobot {
 				y *= state ? 0.8f : -0.8f;
 			}
 		}
-		drive.tankDrive(y - x, y + x);
+		drive.tankDrive((y - x) * 0.3, (y + x) * 0.3); //Add damper for driving demos
 		
 		//--------------- COPILOT CONTROLS ---------------
 		//Ball elevator
